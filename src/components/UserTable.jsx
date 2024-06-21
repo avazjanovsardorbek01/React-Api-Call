1111aa5ab3535e48e134b22301c8e0e98d53ed31
avazjanovsardorbek01 <sardoravazzanov@gmail.com>
2024-06-21 15:19:51 +1000

    .

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -18,6 +18,7 @@ import {
   Button,
   Container,
   Typography,
+  Box,
 } from "@mui/material";
 import { Edit, Delete } from "@mui/icons-material";
 
@@ -62,37 +63,40 @@ const UserTable = () => {
   };
 
   return (
-    <Container>
+    <Container sx={{ paddingLeft: "169px", paddingTop: "2rem" }}>
       <Typography variant="h4" component="h1" gutterBottom>
         User Table
       </Typography>
-      <TableContainer component={Paper}>
+      <TableContainer component={Paper} elevation={3}>
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Username</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Phone</TableCell>
-              <TableCell>Website</TableCell>
-              <TableCell>Actions</TableCell>
+              <TableCell align="center">ID</TableCell>
+              <TableCell align="center">Name</TableCell>
+              <TableCell align="center">Username</TableCell>
+              <TableCell align="center">Email</TableCell>
+              <TableCell align="center">Phone</TableCell>
+              <TableCell align="center">Website</TableCell>
+              <TableCell align="center">Actions</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {users.map((user) => (
               <TableRow key={user.id}>
-                <TableCell>{user.id}</TableCell>
-                <TableCell>{user.name}</TableCell>
-                <TableCell>{user.username}</TableCell>
-                <TableCell>{user.email}</TableCell>
-                <TableCell>{user.phone}</TableCell>
-                <TableCell>{user.website}</TableCell>
-                <TableCell>
-                  <IconButton onClick={() => handleEdit(user)}>
+                <TableCell align="center">{user.id}</TableCell>
+                <TableCell align="center">{user.name}</TableCell>
+                <TableCell align="center">{user.username}</TableCell>
+                <TableCell align="center">{user.email}</TableCell>
+                <TableCell align="center">{user.phone}</TableCell>
+                <TableCell align="center">{user.website}</TableCell>
+                <TableCell align="center">
+                  <IconButton color="primary" onClick={() => handleEdit(user)}>
                     <Edit />
                   </IconButton>
-                  <IconButton onClick={() => handleDelete(user.id)}>
+                  <IconButton
+                    color="secondary"
+                    onClick={() => handleDelete(user.id)}
+                  >
                     <Delete />
                   </IconButton>
                 </TableCell>
@@ -102,61 +106,63 @@ const UserTable = () => {
         </Table>
       </TableContainer>
 
-      <Dialog open={open} onClose={handleClose}>
+      <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
         <DialogTitle>Edit User</DialogTitle>
         <DialogContent>
           <DialogContentText>
             To edit this user, please change the necessary fields and click
             save.
           </DialogContentText>
-          <TextField
-            margin="dense"
-            name="name"
-            label="Name"
-            type="text"
-            fullWidth
-            value={editUser?.name || ""}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="username"
-            label="Username"
-            type="text"
-            fullWidth
-            value={editUser?.username || ""}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="email"
-            label="Email"
-            type="email"
-            fullWidth
-            value={editUser?.email || ""}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="phone"
-            label="Phone"
-            type="text"
-            fullWidth
-            value={editUser?.phone || ""}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="website"
-            label="Website"
-            type="text"
-            fullWidth
-            value={editUser?.website || ""}
-            onChange={handleChange}
-          />
+          <Box component="form" noValidate autoComplete="off" sx={{ mt: 2 }}>
+            <TextField
+              margin="normal"
+              name="name"
+              label="Name"
+              type="text"
+              fullWidth
+              value={editUser?.name || ""}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              name="username"
+              label="Username"
+              type="text"
+              fullWidth
+              value={editUser?.username || ""}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              name="email"
+              label="Email"
+              type="email"
+              fullWidth
+              value={editUser?.email || ""}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              name="phone"
+              label="Phone"
+              type="text"
+              fullWidth
+              value={editUser?.phone || ""}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              name="website"
+              label="Website"
+              type="text"
+              fullWidth
+              value={editUser?.website || ""}
+              onChange={handleChange}
+            />
+          </Box>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={handleClose} color="error">
             Cancel
           </Button>
           <Button onClick={handleSave} color="primary">
